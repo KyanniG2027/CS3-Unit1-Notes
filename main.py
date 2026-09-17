41060dcd965205830236f1b9c39397e6137066b3
Kyanni <kyanni.gonzalez2027@gbwl.org>
2026-09-17 16:34:18 +0000

    Notes on Java and Python

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ def main():
 
 if __name__ == "__main__":
     main()
+        #NOTES
+  
+    #Java int x=5; Phython x=5
+    #Java is strictly typed while phython is dynamic 
+    #Java moreThanOneWord VS PHYTHON underscore more_than_one_word
+
+     # Python comments 
+     #not allowed: 
+     # -to start with numbers 
+     # special char
+     #Keywords: and, if, True, False 
+     #careful: int, list, string (can be overwritten) 
+        #int+int=int or int/int=float or int + float = float
+     #NUMBERS in phython int x=500 
+     #floats: x=500.1
+     #Complex x=30j
+     #x=75 float(75)
+     #print("your grade us:")
+        #print(int(grade))
+
