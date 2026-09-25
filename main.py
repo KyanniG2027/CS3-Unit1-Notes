427883f0c088f9b013dddd667c6c94314886f6f7
Kyanni <kyanni.gonzalez2027@gbwl.org>
2026-09-25 15:48:12 +0000

    Third day

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 #main is defined with no arguments 
 #def name_of_function();
 #indent for code that belongs to function
+#return  --> this is optional. If we dont write return, it will automatically return None
 
-def main():
-    class_size= 7
-    print("hello class of " + str(class_size)+ "!")
+
+#def main():
+    #class_size= 7
+    #print("hello class of " + str(class_size)+ "!")
     #single apostrophes
-    print('hello world')
+    #print('hello world')
     #triple quotes 
-    print("""hello world""")
+   # print("""hello world""")
 
     #printing with fStrings
-    print(f"hello class of {class_size}!")
+    #print(f"hello class of {class_size}!")
     #use brackets when working with fStrings
-    _name="nina"
-    _rent=8000
-    print(f"Hello my name is {_name} and I pay {_rent/30} in rent per day")
-
-if __name__ == "__main__":
-    main()
-        
+    #_name="nina"
+   # _rent=8000
+    #print(f"Hello my name is {_name} and I pay {_rent/30} in rent per day")
+ 
     
         #NOTES
   
@@ -40,23 +39,49 @@ if __name__ == "__main__":
      #x=75 float(75)
      #print("your grade us:")
         #print(int(grade))
-    def main():
+
+def name_of_function():
+    #sample function to show structure 
+    print("good example!")
+
+
+def function_with_args(name):
+    print(f"Hello, thank you for your focus {name}")
+
+
+    #we pass arugemnts to out function
+def greeting(name):
+    return f"Hello,{name}"
+
+def make_a_fraction(x,y=1):
+    return f"{x} / {y}"
+
+def put_under_one(y, x=1):
+     return f"{x}/{y}"
+
+def put_under_one_alt(x): 
+     return f"1/{x}"
+
+
+
+def main():
+        name = "kyanni" 
+        other_name="KG"
+        function_with_args(name)
+        function_with_args(other_name)
+        
         number = "5.0" 
         print(type(int(float(number))))
         print(type(number))
         print(f"Our output is{number}")
         name_of_function()
 
-    def name_of_function():
-        #sample function to show structure 
-        print("good example!")
+        #We can store return output into variables for later
+        my_greeting = greeting("Ms.Dinko")
+        print(my_greeting)
 
-    def function_with_args(name):
-        print(f"Hello, thank you for your focus {name}")
+        print(make_a_fraction(15,4))
+        make_a_fraction(15,4)
 
-    def main():
-        name = "kyanni" 
-        other_name="KG"
-        function_with_args(name)
-        function_with_args(other_name)
-    
+if __name__ == "__main__":
+     main()
